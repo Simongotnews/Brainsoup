7eba0c5c379901258ef589d8d3810bdb9ec8212d
Simongotnews <s.huebner94@gmx.de>
2016-06-16 12:41:35 +0200

    BootStrap mit Member

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,9 +1,11 @@
 import brainsoup.Account
+import brainsoup.Member
 
 class BootStrap {
 
     def init = { servletContext ->
         new Account(username: "Simongotnews", password: "Computer").save()
+        new Member(title: "Herr",firstName: "Simon", lastName: "Huebner", dateOfBirth: new java.util.Date()-1, email: "s.huebner94@gmx.de", user: 1 ).save()
     }
     def destroy = {
     }
