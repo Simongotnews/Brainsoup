b44a4c42396e163d24c060766a4b11f82c136ee9
Simongotnews <s.huebner94@gmx.de>
2016-06-19 19:33:53 +0200

    Logout und Authenticat von ueberall

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,11 +1,12 @@
-import brainsoup.Account
-import brainsoup.Member
+import brainsoup.*
 
 class BootStrap {
 
     def init = { servletContext ->
         new Account(username: "Simongotnews", password: "Computer").save()
         new Member(title: "Herr",firstName: "Simon", lastName: "Huebner", dateOfBirth: new java.util.Date()-1, email: "s.huebner94@gmx.de", user: 1 ).save()
+        new Project(name: "RoboPi", rating: "0", shortDescription: "ferngesteuerter Roboter mit Coilgun.", genre: "electronic", recension: "gut gemacht.", written:  new java.util.Date()-1, member: 1).save()
+        new ProjectPage(longDescription: "Das Chassis muss gebaut werden, damit wir grob wissen was wie und wo hinein passt.", instructions: "1. Gehaeuse besorgen. 2. Zusammenbauen. 3.Bauteile einlegen.", picture: "/sbadmin/images/Brainsoup_LOGO_25x44.png", project: 1).save()
     }
     def destroy = {
     }
